1cc96b0f6145d28e85a3bb19206e1b47937db2ac
Emmanuel-Services <68870605+Emmanuel-Services@users.noreply.github.com>
2022-10-17 04:38:04 +0100

    Update on script.js
    Created diff codes for diff modals

--- js/script.js
+++ js/script.js
@@ -8,63 +8,28 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-var codes = new Array(
-    72627,
-    83637,
-    82725,
-    73528,
-    62424,
-    12526,
-    42426,
-    00590,
-    16760,
-    20605,
-    67600,
-    50000,
-    14736,
-    00900,
-    00100,
-    62628,
-    52526,
-    81648,
-    81645,
-    54645,
-    61246,
-    12137,
-    31575,
-    21879,
-    31945,
-    52525,
-    08267,
-    61218,
-    05484,
-    12485,
-    64218,
-    54648,
-    21248,
-    21576,
-    61976,
-    64246,
-    64645,
-    31675,
-    64667,
-    51646,
-    13787,
-    147369,
-)
-for (let k =0;k < codes.length;k++) {
-    function confirm() {
-        var a = document.getElementsByName("code")[0].value;
-        if(a == codes[k] ) {
+var codes = 72627;
+var ebook= 55667;
+var courses = 77617;
+function confirm() {
+    var a = document.getElementsByName("code")[0].value;
+    if(a == codes ) {
         let replace = document.getElementById("replace");
         replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://www.drive.google.com/drive/folders/1qkcGbbctHNuNfOHsJ-RYnpyJH8Fv7tPP?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
-        console.log('Access Granted')
-        } else {
+        console.log('Access Granted');
+    } else if (a == courses){
+        let replace = document.getElementById("replacez");
+        replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://drive.google.com/drive/folders/1J5ahq829SxaF8fEedilZgR6YkmbBZfZD?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
+        console.log('Access Granted');
+    } else if (a == ebook) {
+        let replace = document.getElementById("replacey");
+        replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://drive.google.com/drive/folders/1JNpoJzIStNyvp_2RNv-STU1aHDnr5H2X?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
+        console.log('Access Granted');
+    } else {
         let replace = document.getElementById("replace");
-        replace.innerHTML = '<br><br><p>Incorrect Please Check The Code</p>';
-        }
-        
+        replace.innerHTML = '<br><br><p>Incorrect Code Please Check The Code</p>';
     }
+        
 }
 
 var image = ["images/Screenshot_20220921-045918_1.png"];

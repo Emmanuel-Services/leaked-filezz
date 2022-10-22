786b43882e049dcbd8ea11f63b97d7d400977c51
Emmanuel-Services <68870605+Emmanuel-Services@users.noreply.github.com>
2022-10-22 04:37:12 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,41 +13,30 @@ var ebook= 55667;
 var courses = 77617;
 function confirm() {
     var a = document.getElementsByName("code")[0].value;
-    if(a == codes ) {
+    if(a === codes) {
         let replace = document.getElementById("replace");
         replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://www.drive.google.com/drive/folders/1qkcGbbctHNuNfOHsJ-RYnpyJH8Fv7tPP?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
         console.log('Access Granted');
-    } else if (a == courses){
+    } else if (a === courses){
         let replace = document.getElementById("replacez");
         replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://drive.google.com/drive/folders/1J5ahq829SxaF8fEedilZgR6YkmbBZfZD?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
         console.log('Access Granted');
-    } else if (a == ebook) {
+    } else if (a === ebook) {
         let replace = document.getElementById("replacey");
         replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://drive.google.com/drive/folders/1JNpoJzIStNyvp_2RNv-STU1aHDnr5H2X?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
         console.log('Access Granted');
-    } else {
+    } else if (a != codes) {
         let replace = document.getElementById("replace");
         replace.innerHTML = '<br><br><p>Incorrect Code Please Check The Code</p>';
+    } else if (a != ebook) {
+        let replace = document.getElementById("replacey");
+        replace.innerHTML = '<br><br><p>Incorrect Code Please Check The Code</p>';
+    } else if (a != courses) {
+        let replace = document.getElementById("replacez");
+        replace.innerHTML = '<br><br><p>Incorrect Code Please Check The Code</p>';
+    } else {
+        let replace = document.getElementById("replace");
+        replace.innerHTML = '<br><br><p>Bro, Don't Stress Me Abeg.</p>';
     }
         
-}
-
-var image = ["images/Screenshot_20220921-045918_1.png"];
-var num =0;
-function next() {
-    var slider = document.getElementById("slider");
-    num++;
-    if (num>= image.lenght) {
-        num =0;
-    }
-    slider.src = image[num];
-}
-function prev() {
-    var slider = document.getElementById("slider");
-    num--;
-    if (num < 0) {
-        num = image.lenght - 1;
-        num =0;
-    }
-    slider.src = image[num];
 }

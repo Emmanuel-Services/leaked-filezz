bf8f3b6ecc21fb3e4a25f1fe1b9bf36b5f739cdc
Emmanuel-Services <68870605+Emmanuel-Services@users.noreply.github.com>
2022-11-03 00:10:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,15 +16,15 @@ function confirm() {
     if(a === codes) {
         let replace = document.getElementById("replace");
         replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://www.drive.google.com/drive/folders/1qkcGbbctHNuNfOHsJ-RYnpyJH8Fv7tPP?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
-        console.log('Access Granted');
+        
     } else if (a === courses){
         let replace = document.getElementById("replacez");
         replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://drive.google.com/drive/folders/1J5ahq829SxaF8fEedilZgR6YkmbBZfZD?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
-        console.log('Access Granted');
+        
     } else if (a === ebook) {
         let replace = document.getElementById("replacey");
         replace.innerHTML = '<br><p>Code Correct Use The Button Below to Download Via Google Drive</p><br><a href="https://drive.google.com/drive/folders/1JNpoJzIStNyvp_2RNv-STU1aHDnr5H2X?usp=sharing" class="btn btn-lg btn-success" target="_blank" style="width:98%; padding:5px;">Download</a>';
-        console.log('Access Granted');
+        
     } else if (a != codes) {
         let replace = document.getElementById("replace");
         replace.innerHTML = '<br><br><p>Incorrect Code Please Check The Code</p>';
